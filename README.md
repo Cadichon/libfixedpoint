21d0270384af9c51a68eb72aaec9ade42b386174
Léo Labruyère <leo.labruyere@pm.me>
2020-03-07 14:33:56 -0500

    Finished

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# libfixedpoint
+## Partie 1
+
+Pour lancer se projet, il vous faut deux programme :
+- dub (version >= 1.19)
+- dmd (version >= 2.090.1)
+
+Pour les obtenir sur Ubuntu/Debian
+
+``` bash
+sudo wget https://netcologne.dl.sourceforge.net/project/d-apt/files/d-apt.list -O /etc/apt/sources.list.d/d-apt.list
+sudo apt-get update --allow-insecure-repositories
+sudo apt-get --allow-unauthenticated install d-apt-keyring
+sudo apt-get update && sudo apt-get install dmd-compiler dub
+```
+
+Pour plus d'options: https://dlang.org/download.html
+
+Une fois installé,
+
+``` bash
+dub test
+```
+
+Cela compilera la libfixedpoint et lancera les tests unitaires sur les opérations basiques (+, -, * et /)
+Les tests unitaires utiliserons la méthode "toString" pour afficher les résultats des différentes opération
+
+## Partie 2
+Les tests unitaire testerons aussi la méthode de Newton sur 15
+
+## Partie 3
+Une partie décimal plus petite, dans une représentation d'un nombre en point fixe, permet moins de liberté quand a la précision représentable
+On peut voir cela lorsque l'on essaye de convertir "68.2" en représentation en point fixe, avec une partie décimal sur 4 bits.
+On ne peut pas obtenir 0.2 en additionnant des puissances de deux négatives entre -1 et -4.
+C'est pour cela que, dans les tests unitaires, "62.8" devient "62.1875".
+
+Lorsque l'on applique cela a la méthode de Newton, chaque itération de cette derniere nécessite de plus en plus de précision apres la virgule,
+qui ne peut pas etre représentée avec une partie décimal représenter sur 4 bits.
+
+Les deux derniers tests unitaire test la méthode de Newton sur 15, avec respectivement 8 et 16 bits de précision.
+On voit bien que les resultats de cette derniere, sur des nombres pouvant représenter une partie décimal plus diversifié, sont plus proches du vrai resultat.
--- source/fixedpoint.d
+++ source/fixedpoint.d
@@ -5,6 +5,7 @@ private import std.stdio;
 private import std.format;
 private import std.conv;
 private import std.range;
+private import std.algorithm;
 private import std.exception;
 private import std.math;
 
@@ -33,10 +34,11 @@ private:
   }
 
   T floatingTypeToInternalType(const(real) floating) const {
-    real tmp = floating * (1 << precision);
-    T ret = cast(T)(tmp);
+    if (isNaN(floating) || isInfinity(floating))
+      throw new ConvException(to!string(floating) ~ " is not a valid floating point number");
+    real tmp = round(floating * (2 ^^ precision));
 
-    return ret;
+    return cast(T)(tmp);
   }
 
 public:
@@ -81,20 +83,20 @@ public:
 
   /* Opérateur add, sub, mul, div, mod */
   typeof(this) opBinary(string op)(const(typeof(this)) rhs) const {
-    Unconst!(typeof(this)) res; //this est constant a cause du const de la methode, Unconst permet de déclarer une variable du même type que this, mais non constant
+    Unconst!(typeof(this)) res;
 
     static if (op == "+") {
-      res.internal = cast(T)(this.internal + rhs.internal);
+      res.internal = cast(T)(cast(ulong)(this.internal) + cast(ulong)(rhs.internal));
     }
     else static if (op == "-") {
-      res.internal = cast(T)(this.internal - rhs.internal);
+      res.internal = cast(T)(cast(ulong)(this.internal) - cast(ulong)(rhs.internal));
     }
     else static if (op == "*") {
-      res.internal = cast(T)((this.internal * rhs.internal) >> precision);
+      res.internal = cast(T)((cast(ulong)(this.internal) * cast(ulong)(rhs.internal)) / (2 ^^ precision));
     }
-    /*
-      TODO: implement /
-    */
+    else static if (op == "/") {
+      res.internal = cast(T)((cast(ulong)(this.internal) * (2 ^^ precision) / cast(ulong)(rhs.internal)));
+    }
     else {
       static assert(false, op ~ " not implemented for " ~ typeof(this).stringof);
     }
@@ -103,17 +105,20 @@ public:
 
   typeof(this) opBinary(string op)(const(ulong) rhs) const {
     Unconst!(typeof(this)) res = rhs;
+
     return opBinary!(op)(res);
   }
 
   typeof(this) opBinary(string op)(const(string) rhs) const {
     Unconst!(typeof(this)) res;
+
     res.internal = binaryStringToInternalType(rhs);
     return opBinary!(op)(res);
   }
 
   typeof(this) opBinary(string op)(const(real) rhs) const {
     Unconst!(typeof(this)) res;
+
     res.internal = floatingTypeToInternalType(rhs);
     return opBinary!(op)(res);
   }
@@ -143,12 +148,14 @@ public:
 
   bool opEquals(const(string) rhs) const {
     Unconst!(typeof(this)) res;
+
     res.internal = binaryStringToInternalType(rhs);
     return opEquals(res);
   }
 
   bool opEquals(const(real) rhs) const {
     Unconst!(typeof(this)) res;
+
     res.internal = floatingTypeToInternalType(rhs);
     return opEquals(res);
   }
@@ -164,12 +171,14 @@ public:
 
   int opCmp(const(string) rhs) const {
     Unconst!(typeof(this)) res;
+
     res.internal = binaryStringToInternalType(rhs);
     return (cast(int)(this.internal - res.internal));
   }
 
   int opCmp(const(real) rhs) const {
     Unconst!(typeof(this)) res;
+
     res.internal = floatingTypeToInternalType(rhs);
     return (cast(int)(this.internal - res.internal));
   }
@@ -188,23 +197,44 @@ public:
 
     for (int i; i < precision; i += 1) {
       int pos = precision - i - 1;
-      real fact = (fracBits & (1 << pos)) >> pos;
+      real fact = ((fracBits >> pos) & 1);
 
       tmp += ldexp(fact, - i - 1);
     }
     do {
       fracPart = modf(tmp, intPart);
       tmp *= 10;
-    } while (fracPart < 0);
-    return cast(T)(tmp);
+    } while (fracPart != 0);
+    return cast(T)(intPart);
   }
 
   string toString() const {
     return to!string(this.integralPart) ~ "." ~ to!string(this.fractionalPart);
   }
+
+  string toBinary(bool addPoint = false) const {
+    char[] ret;
+    int i;
+
+    for (i = T.sizeof * 8 - 1; i >= 0; i -= 1) {
+      ubyte bit = cast(ubyte)((this.internal >> i) & 1);
+
+      ret ~= cast(char)(bit + '0');
+      if (addPoint && i ==  precision)
+        ret ~= '.';
+    }
+    return ret;
+  }
 }
 
 public alias fixed = FixedImpl!(ushort, 4);
+
+fixed newton(ubyte nbRec)(fixed n) {
+  static if (nbRec == 0)
+    return n;
+  else
+    return fixed(0.5) * (newton!(nbRec - 1)(n) + (n / newton!(nbRec - 1)(n)));
+}
 
 unittest {
   import std.stdio;
@@ -222,7 +252,9 @@ unittest {
   import std.stdio;
 
   writeln("Testing int initialisation and assignment");
+  writeln("\tfixed test = 42");
   fixed test = 42;
+  writefln("\ttest == %s", test);
   assert(test == 42);
 }
 
@@ -230,44 +262,129 @@ unittest {
   import std.stdio;
 
   writeln("Testing binary string initialisation and assignment");
+  writeln("\tfixed test = \"1010100000\"");
   fixed test = "1010100000";
-  assert(test == 42);
+  writefln("\ttest == %s", test);
+  assert(test == 42.0);
 }
 
 unittest {
   import std.stdio;
 
   writeln("Testing float initialisation and assignment");
-  fixed test = 42.0f;
-  assert(test == 42); //Conversion très imprécise pour les nombre "vraiment" flottant
+  writeln("\tfixed test = 42.5");
+  fixed test = 42.5;
+  writefln("\ttest == %s", test);
+  assert(test == 42.5);
+  writeln("\tfixed test = 68.2");
+  fixed test2 = 68.2;
+  writefln("\ttest2 == %s", test2);
+  assert(test2 == 68.1875);
+  writeln("\t\\\\precision problem expected, a 4 bits fractionnal part can't stock 0.2, 0.1875 is the closest thing it can handle");
 }
 
 unittest {
   import std.stdio;
 
   writeln("Testing addition");
-  fixed test = 42;
-  fixed test2 = "1010101000"; //42.5
-  assert(test + 2 == 44);
-  assert((test + "1010101000").toString == "84.5");
-  assert(test + 2f == 44); //Conversion très imprécise pour les nombre "vraiment" flottant
+  writefln("\tfixed(42) + fixed(2) == %s", fixed(42) + fixed(2));
+  assert(fixed(42) + fixed(2) == fixed(44));
+  writefln("\tfixed(42) + fixed(\"1010101000\") == %s", fixed(42) + fixed("1010101000"));
+  assert(fixed(42) + fixed("1010101000") == 84.5);
+  writeln("\tfixed(42) + 2.5 == %s", fixed(42) + 2.5);
+  assert(fixed(42) + 2.5 == 44.5);
 }
 
 unittest {
   import std.stdio;
 
   writeln("Testing substraction");
-  fixed test = 82;
-  fixed test2 = "1010101000"; //42.5
-  assert(test - 2 == 80);
-  assert((test - "1010101000").toString == "39.5");
-  assert(test - 2f == 80); //Conversion très imprécise pour les nombre "vraiment" flottant
+  writefln("\tfixed(84) - fixed(42) == %s", fixed(84) - fixed(42));
+  assert(fixed(84) - fixed(42) == fixed(42));
+  writefln("\tfixed(42.625) - fixed(0.625) == %s", fixed(42.625) - fixed(0.625));
+  assert(fixed(42.625) - fixed(0.625) == 42);
 }
 
 unittest {
-  fixed test = 82;
-  fixed test2 = "1010101000"; //42.5
-  assert(test * test2 == 3485);
-  assert(fixed("10000") * fixed("1010100100")/*1 * 42.25*/ == "1010100100");
-  assert(fixed("110000") * fixed("1010100100")/*3 * 42.25*/ == "11111101100" /*126.75*/);
+  writeln("Testing multiplication");
+  writefln("\tfixed(82) * fixed(42.5) == %s", fixed(82) * fixed(42.5));
+  assert(fixed(82) * fixed(42.5) == 3485);
+  writefln("\tfixed(\"10000\") * fixed(\"1010100100\") == %s", fixed("10000") * fixed("1010100100"));
+  assert(fixed("10000") * fixed("1010100100") == "1010100100");
+  writefln("\tfixed(\"110000\") * fixed(\"1010100100\") == %s", fixed("110000") * fixed("1010100100"));
+  assert(fixed("110000") * fixed("1010100100") == "11111101100");
+  writefln("\tfixed(1) * fixed(42.25) == %s", fixed(1) * fixed(42.25));
+  assert(fixed(1) * fixed(42.25) == fixed(42.25));
+  writefln("\tfixed(3) * fixed(42.25) == %s", fixed(3) * fixed(42.25));
+  assert(fixed(3) * fixed(42.25) == fixed(126.75));
+}
+
+unittest {
+  writeln("Testing division");
+  assert(fixed(15) / fixed(15) == 1);
+  writefln("\tfixed(15) / fixed(15) == %s", fixed(15) / fixed(15));
+  assert(fixed(42) / fixed(3) == 14);
+  writefln("\tfixed(42) / fixed(3) == %s", fixed(42) / fixed(3));
+  assert(fixed(42) / fixed(3.5) == 12);
+  writefln("\tfixed(42) / fixed(3.5) == %s", fixed(42) / fixed(3.5));
+
+}
+
+unittest {
+  writeln("Testing newton");
+  assert(newton!(0)(fixed(15)) == 15);
+  assert(newton!(1)(fixed(15)) == 8);
+  assert(newton!(2)(fixed(15)) == 4.9375);
+  assert(newton!(3)(fixed(15)) == 3.9375);
+  assert(newton!(4)(fixed(15)) == 3.8125);
+  assert(newton!(5)(fixed(15)) == 3.8125);
+
+  writefln("\tnewton!(0)(fixed(15)) = %s", newton!(0)(fixed(15)));
+  writefln("\tnewton!(1)(fixed(15)) = %s", newton!(1)(fixed(15)));
+  writefln("\tnewton!(2)(fixed(15)) = %s", newton!(2)(fixed(15)));
+  writefln("\tnewton!(3)(fixed(15)) = %s", newton!(3)(fixed(15)));
+  writefln("\tnewton!(4)(fixed(15)) = %s", newton!(4)(fixed(15)));
+  writefln("\tnewton!(5)(fixed(15)) = %s", newton!(5)(fixed(15)));
+}
+
+
+unittest {
+  writeln("Testing newton with fixed 32 bits with 8 bits precision");
+
+  alias lfixed = FixedImpl!(uint, 8);
+
+  lfixed lnewton(ubyte nbRec)(lfixed n) {
+    static if (nbRec == 0)
+      return n;
+    else
+      return lfixed(0.5) * (lnewton!(nbRec - 1)(n) + (n / lnewton!(nbRec - 1)(n)));
+  }
+
+  writefln("\tlnewton!(0)(lfixed(15)) = %s", lnewton!(0)(lfixed(15)));
+  writefln("\tlnewton!(1)(lfixed(15)) = %s", lnewton!(1)(lfixed(15)));
+  writefln("\tlnewton!(2)(lfixed(15)) = %s", lnewton!(2)(lfixed(15)));
+  writefln("\tlnewton!(3)(lfixed(15)) = %s", lnewton!(3)(lfixed(15)));
+  writefln("\tlnewton!(4)(lfixed(15)) = %s", lnewton!(4)(lfixed(15)));
+  writefln("\tlnewton!(5)(lfixed(15)) = %s", lnewton!(5)(lfixed(15)));
+
+}
+unittest {
+  writeln("Testing newton with fixed 64 bits with 16 bits precision");
+
+  alias vlfixed = FixedImpl!(ulong, 16);
+
+  vlfixed vlnewton(ubyte nbRec)(vlfixed n) {
+    static if (nbRec == 0)
+      return n;
+    else
+      return vlfixed(0.5) * (vlnewton!(nbRec - 1)(n) + (n / vlnewton!(nbRec - 1)(n)));
+  }
+
+  writefln("\tvlnewton!(0)(lfixed(15)) = %s", vlnewton!(0)(vlfixed(15)));
+  writefln("\tvlnewton!(1)(lfixed(15)) = %s", vlnewton!(1)(vlfixed(15)));
+  writefln("\tvlnewton!(2)(lfixed(15)) = %s", vlnewton!(2)(vlfixed(15)));
+  writefln("\tvlnewton!(3)(lfixed(15)) = %s", vlnewton!(3)(vlfixed(15)));
+  writefln("\tvlnewton!(4)(lfixed(15)) = %s", vlnewton!(4)(vlfixed(15)));
+  writefln("\tvlnewton!(5)(lfixed(15)) = %s", vlnewton!(5)(vlfixed(15)));
+
 }
